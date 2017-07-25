880315501c81bcb490e9bb480ba58a0b6d0a79f6
Auburngrads <auburngrads@live.com>
2017-07-24 20:11:00 -0400

    Update pkg teachingSMRD

---
+++ teachingSMRD/apps/table3_5/ui.R
@@ -0,0 +1,10 @@
+ui = navbarPage(title = 'Table 3.5',
+                collapsible = T, 
+                position = 'fixed-top',
+                theme  = teachingApps::add_theme(getShinyOption('theme')),
+                header = teachingApps::add_style(),
+                footer = teachingApps::add_stamp(),
+
+                
+    tabPanel(h4('Table 3.5'), DT::dataTableOutput('table5')))
+
