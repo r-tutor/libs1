c8080224d8ea89dfc358879ac071af5f4d9f426d
Auburngrads <auburngrads@live.com>
2017-09-24 19:50:51 -0400

    Update Packages-4

--- showtext/doc/introduction.R
+++ showtext/doc/introduction.R
@@ -1,11 +1,11 @@
 ## ----message = FALSE, fig.align='center', fig.showtext=TRUE--------------
 library(showtext)
 ## Loading Google fonts (http://www.google.com/fonts)
-font.add.google("Gochi Hand", "gochi")
-font.add.google("Schoolbell", "bell")
+font_add_google("Gochi Hand", "gochi")
+font_add_google("Schoolbell", "bell")
 
 ## Automatically use showtext to render text
-showtext.auto()
+showtext_auto()
 
 set.seed(123)
 ## Manually open a graphics device if you run this code in RStudio
@@ -16,16 +16,16 @@ title("Histogram of Normal Random Numbers", family = "bell", cex.main = 2)
 title(ylab = "Frequency", family = "gochi", cex.lab = 2)
 text(2, 70, "N = 1000", family = "bell", cex = 2.5)
 
-## ----echo=FALSE, fig.align='center', fig.showtext=TRUE-------------------
+## ----echo=FALSE, message=FALSE, fig.align='center', fig.showtext=TRUE----
 showtext.end()
 
 set.seed(123)
 hist(rnorm(1000), breaks = 30, col = "steelblue", border = "white",
      main = "Histogram of Normal Random Numbers", xlab = "", ylab = "Frequency")
 
-showtext.begin()
+showtext_begin()
 text(2, 70, "N = 1000", family = "bell", cex = 2.5)
-showtext.end()
+showtext_end()
 
-showtext.begin()
+showtext_begin()
 
