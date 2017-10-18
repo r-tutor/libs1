17f7e5951b29e335142a4d17a6df63a53d42d93a
Auburngrads <auburngrads@live.com>
2017-10-18 15:41:57 -0400

    Update Packages-4

--- rJava/jri/run.bat
+++ rJava/jri/run.bat
@@ -1,2 +1,2 @@
-set PATH=%PATH%;C:/PROGRA~1/R/R-34~1.2\bin;C:/PROGRA~1/R/R-34~1.2\lib
-C:/PROGRA~1/Java/JDK18~1.0_1/bin/java -cp .;examples;JRI.jar;src/JRI.jar rtest $*
+set PATH=%PATH%;D:/RCompile/recent/R-34~1.2\bin;D:/RCompile/recent/R-34~1.2\lib
+c:/Progra~2/Java/JDK18~2.0_1/bin/java -cp .;examples;JRI.jar;src/JRI.jar rtest $*
